a398107be9e7c4cf2b0f5aad7ff8dea7b3bd69c8
Zamirathe <zamirathe@centurylink.net>
2015-07-29 19:48:10 -0600

    Fixed to override other Text Commands, and unloads properly

--- ZaupTextCommands.cs
+++ ZaupTextCommands.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Rocket.API;
+using Rocket.Core;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
 using Rocket.Unturned;
-using Rocket.Unturned.Logging;
+using Rocket.Unturned.Chat;
 using Rocket.Unturned.Plugins;
 using SDG.Unturned;
 using Steamworks;
@@ -16,41 +20,21 @@ namespace ZaupTextCommands
     {
         protected override void Load()
         {
-            if (this.Configuration.commands.Count > 0)
+            if (this.Configuration.Instance.commands.Count > 0 && this.Configuration != null && this.Configuration.Instance.commands != null)
             {
-                foreach (TextCommand command in this.Configuration.commands)
+                foreach (ZaupTextCommand command in this.Configuration.Instance.commands)
                 {
-                    List<Command> list = new List<Command>();
-                    Command zcommand = new ZaupTextCommand(command.Name, command.Help, command.Text);
-                    List<Command> commands = Commander.Commands;
-                    bool flag = false;
-                    for (int i = 0; i < commands.Count; i++)
-                    {
-                        Command command2 = commands[i];
-                        if (command2.commandName.ToLower().Equals(zcommand.commandName.ToLower()))
-                        {
-                            Logger.LogWarning(string.Concat(new string[]
-				            {
-					        "Couldn't register RocketTextCommand.",
-					        zcommand.commandName,
-					        " because it would overwrite ",
-					        command2.GetType().Assembly.GetName().Name,
-					        ".",
-					        command2.commandName
-				            }));
-                            flag = true;
-                        }
-                        else
-                        {
-                            list.Add(command2);
-                        }
-                    }
-                    if (!flag)
-                    {
-                        // We add the command.
-                        Logger.Log("Added command ZaupTextCommand." + zcommand.commandName);
-                        Commander.register(zcommand);
-                    }
+                    Commander.register(command);
+                }
+            }
+        }
+        protected override void Unload()
+        {
+            if (this.Configuration.Instance.commands.Count > 0 && this.Configuration != null && this.Configuration.Instance.commands != null)
+            {
+                foreach (ZaupTextCommand command in this.Configuration.Instance.commands)
+                {
+                    Commander.deregister(command);
                 }
             }
         }
@@ -65,11 +49,13 @@ namespace ZaupTextCommands
 			this.commandInfo = this.commandName + " - " + this.commandHelp;
 			this.text = text;
 		}
+        public ZaupTextCommand()
+        { }
 		protected override void execute(CSteamID caller, string command)
 		{
 			foreach (string str in this.text)
 			{
-				RocketChat.Say(caller, str);
+				UnturnedChat.Say(caller, str);
 			}
 		}
     }
